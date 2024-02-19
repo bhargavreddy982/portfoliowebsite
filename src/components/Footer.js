08536633890a9fa66679fbd867cf9e780755fd87
bhargavreddy982 <102218460+bhargavreddy982@users.noreply.github.com>
2024-02-19 17:28:39 +0530

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,7 @@ import React from "react";
 import InstagramIcon from "@material-ui/icons/Instagram";
 import TwitterIcon from "@material-ui/icons/Twitter";
 import FacebookIcon from "@material-ui/icons/Facebook";
-import LinkedInIcon from "@material-ui/icons/LinkedIn";
+
 import "../styles/Footer.css";
 
 function Footer() {
@@ -10,7 +10,7 @@ function Footer() {
     <div className="footer">
       <div className="socialMedia">
        
-        <LinkedInIcon />
+        
       </div>
       <p> bhargavreddy.g20@iiits.in</p>
     </div>
